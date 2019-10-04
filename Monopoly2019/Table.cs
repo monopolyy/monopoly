640a7a9669c7b81d41f0d73f13125508c7a9ba46
tomasjur <tomasjkaunas@gmail.com>
2019-10-04 23:54:08 +0300

    Added Singleton class - Table.

---
+++ Monopoly2019/Table.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Singleton class for Table. Only one table will be in the whole game.
+namespace Monopoly2019
+{
+    public sealed class Table
+    {
+        private static Table table = null;
+        private static readonly object padlock = new object();
+
+        Table()
+        { }
+
+        public static Table Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (table == null)
+                    {
+                        table = new Table();
+                    }
+                    return table;
+                }
+            }
+        }
+    }
+}
