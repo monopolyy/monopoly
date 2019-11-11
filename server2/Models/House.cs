ee719a9ed91e65d44151d43073686e34578a18db
tomasjur <tomasjkaunas@gmail.com>
2019-11-11 18:44:49 +0200

    prototype krw

--- server2/Models/House.cs
+++ server2/Models/House.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace server2.Builder
+namespace server2.Builder // and Prototype too
 {
     public class House
     {
@@ -17,6 +17,35 @@ namespace server2.Builder
             Price = price;
             Billing = billing;
             NumberOfHouses = numberofhouses;
+        }
+        // Keisis jeigu pasikeis šablonas
+        public House ShallowCopy()
+        {
+            return (House)this.MemberwiseClone();
+        }
+        // Keičia tik tas reikšmes, kurios yra užpildytos per parametrus
+        // -1 arba "" reiškia, kad reikšmės keisti nereikia
+        public House DeepCopy(string changeName, int changePrice, int changeBilling,
+            int changeNumberofhouses)
+        {
+            House clone = (House)this.MemberwiseClone();
+            if (changeName != "")
+            {
+                clone.Name = changeName;
+            }
+            if (changePrice > -1)
+            {
+                clone.Price = changePrice;
+            }
+            if (changeBilling > -1)
+            {
+                clone.Billing = changeBilling;
+            }
+            if (changeNumberofhouses > -1)
+            {
+                clone.NumberOfHouses = changeNumberofhouses;
+            }
+            return clone;
         }
     }
 }
